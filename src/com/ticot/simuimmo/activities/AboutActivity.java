92912054cedb94ec4bd7d066b403e892fddcbdd0
TAurelien <aurelien.ticot@gmail.com>
2013-05-03 19:25:11 +0200

    Update the app_ver to get the string from ressources
    Change the harcoded string by a the "about_version" string from
    ressources
    
    Signed-off-by: TAurelien <aurelien.ticot@gmail.com>

--- src/com/ticot/simuimmo/activities/AboutActivity.java
+++ src/com/ticot/simuimmo/activities/AboutActivity.java
@@ -1,13 +1,14 @@
 /*
  * Copyright (C) 2013 Aurelien Ticot
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You
- * may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
  * 
  * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
- * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
 
@@ -28,6 +29,7 @@ import com.ticot.simuimmo.R;
  */
 public class AboutActivity extends Activity {
 	
+	
 	private static final String tag = "LOG";
 	
 	@Override
@@ -37,11 +39,11 @@ public class AboutActivity extends Activity {
 		
 		setContentView(R.layout.activity_about);
 		
-		//TODO Try to add non-harcoded text
-		String app_ver = "Version ";
+		String app_ver = getString(R.string.about_version);
 		
 		try {
-			app_ver = app_ver + getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+			app_ver = app_ver + " "
+					+ getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
 		}
 		catch (final NameNotFoundException e) {
 			Log.v(tag, e.getMessage());
