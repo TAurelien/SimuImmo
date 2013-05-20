7464ea6f6848f09ee0449d568323ec7bfe824456
TAurelien <aurelien.ticot@gmail.com>
2013-05-20 17:21:09 +0200

    Start building the UI of Gestion
    Signed-off-by: TAurelien <aurelien.ticot@gmail.com>

---
+++ src/com/ticot/simuimmo/activities/GestionActivity.java
@@ -0,0 +1,17 @@
+package com.ticot.simuimmo.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.ticot.simuimmo.R;
+
+public class GestionActivity extends Activity {
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+	
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_gestion);
+	}
+	
+}
