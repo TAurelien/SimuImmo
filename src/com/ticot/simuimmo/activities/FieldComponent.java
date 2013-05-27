3275ec40a54fd219fd2d6454fc584ed3a3290597
TAurelien <aurelien.ticot@gmail.com>
2013-05-27 15:52:54 +0200

    Create the compound component for the form field
    Based on the layout component_form_field.xml, creation of the java class
    extending the LinearLayout class.
    + set up of the listener of the checkbox to switch value to real value
    
    Signed-off-by: TAurelien <aurelien.ticot@gmail.com>

---
+++ src/com/ticot/simuimmo/activities/FieldComponent.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2013 Aurelien Ticot
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.ticot.simuimmo.activities;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.LinearLayout;
+import com.ticot.simuimmo.BuildConfig;
+import com.ticot.simuimmo.R;
+
+public class FieldComponent extends LinearLayout implements OnCheckedChangeListener {
+	
+	
+	/**
+	 * 
+	 */
+	private final View CONTAINER;
+	
+	/**
+	 * 
+	 * @param context
+	 * @param attrs
+	 */
+	public FieldComponent(Context context, AttributeSet attrs) {
+	
+		super(context, attrs);
+		final LayoutInflater inflater = (LayoutInflater) context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		CONTAINER = inflater.inflate(R.layout.component_form_field, this);
+		
+		((CheckBox) CONTAINER.findViewById(R.id.fieldReal))
+				.setOnCheckedChangeListener(this);
+		
+	}
+	
+	@Override
+	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+	
+		if (BuildConfig.DEBUG) { //DEBUG
+			Log.d("Testing", "in switch real field");
+		}
+		
+		if (isChecked) {
+			CONTAINER.findViewById(R.id.fieldValue).setVisibility(8);
+			CONTAINER.findViewById(R.id.fieldRealValue).setVisibility(0);
+			CONTAINER.findViewById(R.id.fieldRealValue).requestFocus();
+			CONTAINER.findViewById(R.id.fieldRealValue).setTag("Mandatory");
+		}
+		else {
+			CONTAINER.findViewById(R.id.fieldValue).setVisibility(0);
+			CONTAINER.findViewById(R.id.fieldRealValue).setVisibility(8);
+			CONTAINER.findViewById(R.id.fieldRealValue).setTag("Optional");
+		}
+		
+	}
+	
+}
